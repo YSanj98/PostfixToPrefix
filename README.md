56776570447fa64fc10e415911d9453fa87ef98a
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-01-10 23:26:04 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Postfix To Prefix 
+Convert Postfix Expressiom  To Prefix Expression Using Python
+
+---
+### Author
+
+👤 **Yasindu Sanjeewa**
+
+
+-   Github: [@YSanj98](https://github.com/YSanj98)
+-   LinkedIn: [@yasindu_sanjeewa](https://www.linkedin.com/in/yasindu-sanjeewa-a79783202)
+
+---
+
+### 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check [issues page](https://github.com/MadushaS/MedusaNote/issues)
+
+---
+
+### Show your support
+
+Give a ⭐️ if this project helped you!
+
+---
+
+
---
+++ main.py
@@ -0,0 +1,43 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def is_operand(c):
+    if c.isalpha():
+        return True
+    else:
+        return False
+
+
+def postfixtoprefix(post):
+    stack = []
+    for c in post:
+        if is_operand(c):
+            op = TreeNode(c)
+            stack.append(op)
+        else:
+            op1 = stack.pop()
+            op2 = stack.pop()
+            expression = TreeNode(c)
+            expression.left = op1
+            expression.right = op2
+            stack.append(expression)
+    return stack.pop()
+
+
+def print_prefix_expression(rootelement):
+    if rootelement:
+        print(rootelement.value, end="")
+        print_prefix_expression(rootelement.right)
+        print_prefix_expression(rootelement.left)
+
+
+postfix = input("Enter a valid postfix expression: ")
+print("Postfix expression: ", postfix)
+root = postfixtoprefix(postfix)
+print("Prefix expression: ", end="")
+print_prefix_expression(root)
+
