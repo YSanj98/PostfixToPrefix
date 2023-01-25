fecb203f1f635d6af8ec74e177c0b12e70240d88
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-01-25 14:53:48 +0530

    Add comments for readability

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 class TreeNode:
     def __init__(self, value):
+        """
+        Constructor which sets the value, left and right attributes of the class.
+        """
+
         self.value = value
         self.left = None
         self.right = None
 
 
 def is_operand(c):
+    """
+    Check if a character is an operand or not.
+    """
     return bool(c.isalpha())
 
 
 def postfixtoprefix(post):
+    """
+    Convert a postfix expression to a prefix expression by iterating through the characters in the postfix expression,
+    and using a stack to keep track of the operands and operators.
+    """
     stack = []
     for c in post:
         if is_operand(c):
@@ -26,12 +37,18 @@ def postfixtoprefix(post):
 
 
 def print_prefix_expression(rootelement):
+    """
+    Recursively print out the prefix expression by traversing the tree in a pre-order fashion.
+    """
     if rootelement:
         print(rootelement.value, end="")
         print_prefix_expression(rootelement.right)
         print_prefix_expression(rootelement.left)
 
 try:
+    """
+    Exception Handling
+    """
     postfix = input("Enter a valid postfix expression: ")
     print("Postfix expression: ", postfix)
     root = postfixtoprefix(postfix)
